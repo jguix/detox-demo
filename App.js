608acdeaf3015bd6819028339a30a12b45a19a96
_jordaj3 <juangui.jordan2@takeda.com>
2021-11-01 00:15:45 +0100

    detox configuration

--- App.js
+++ App.js
@@ -6,85 +6,116 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
+  Button,
+  Platform,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
+  const [isMessageVisible, setMessageVisible] = useState(false);
+  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = true;
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    height: '100%',
+  };
+  const textStyle = {
+    color: isDarkMode ? Colors.white : Colors.black,
+  };
+
+  const buttonHandler = async () => {
+    setMessageVisible(true);
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Push notifications">
-            This app can receive push notifications from Firebase.
-          </Section>
+      <View style={styles.container}>
+        <View>
+          <Text testID="title" style={[styles.title, textStyle]}>
+            E2E testing with Detox
+          </Text>
+          <Text
+            testID="description"
+            style={[styles.description, styles.marginTop, textStyle]}>
+            This app is testeable with Detox. Push the button to display a
+            greeting message.
+          </Text>
+          {isMessageVisible && (
+            <Text
+              testID="message"
+              style={[styles.message, styles.marginTop, textStyle]}>
+              Hello world!
+            </Text>
+          )}
         </View>
-      </ScrollView>
+
+        <View style={styles.flex1}></View>
+        <View style={[styles.button, styles.marginTop]}>
+          <Button
+            testID="message_button"
+            color={Platform.OS === 'android' ? 'black' : 'white'}
+            title="Say Hello"
+            onPress={buttonHandler}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
+  button: {
+    marginTop: 12,
+    backgroundColor: 'black',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
     marginTop: 32,
+    paddingBottom: 48,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
+  description: {
+    marginTop: 8,
     fontSize: 24,
+    fontWeight: '400',
+  },
+  flex1: {
+    flex: 1,
+  },
+  marginTop: {
+    marginTop: 24,
+  },
+  message: {
+    fontSize: 96,
     fontWeight: '600',
   },
-  sectionDescription: {
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+  tokenContainer: {
+    borderRadius: 5,
+    borderWidth: 1,
     marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+    padding: 5,
+    fontSize: 12,
+    fontWeight: '300',
   },
 });
 
---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,21 @@
+describe('App', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show a title and a description', async () => {
+    await expect(element(by.id('title'))).toBeVisible();
+    await expect(element(by.id('description'))).toBeVisible();
+    await expect(element(by.id('message'))).not.toBeVisible();
+  });
+
+  it('should show a hello world message after tapping the button', async () => {
+    await element(by.id('message_button')).tap();
+    await expect(element(by.id('message'))).toBeVisible();
+    await expect(element(by.text('Hello world!'))).toBeVisible();
+  });
+});
